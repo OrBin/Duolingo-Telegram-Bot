86126454cf8238511363432b6f1ad8ed10de9847
Or Bin <orbin50@gmail.com>
2016-10-31 14:46:13 +0200

    Initial code commit

---
+++ jobs/notify_users.py
@@ -0,0 +1,81 @@
+"""
+# Partial Duolingo API reference: http://tschuy.com/duolingo/
+
+# Possibly interesting data fields:
+print json_data[u'language_data'][u'ru'][u'language_strength']
+print json_data[u'language_data'][u'ru'][u'level']
+print json_data[u'language_data'][u'ru'][u'level_left']
+print json_data[u'language_data'][u'ru'][u'level_percent']
+print json_data[u'language_data'][u'ru'][u'level_points']
+print json_data[u'language_data'][u'ru'][u'level_progress']
+print json_data[u'language_data'][u'ru'][u'max_depth_learned']
+print json_data[u'language_data'][u'ru'][u'max_tree_level']
+print json_data[u'language_data'][u'ru'][u'num_skills_learned']
+print json_data[u'language_data'][u'ru'][u'points']
+print json_data[u'language_data'][u'ru'][u'streak']
+print json_data[u'language_data'][u'ru'][u'max_tree_level']
+print json_data[u'rupees']
+print json_data[u'site_streak']
+print json_data[u'streak_extended_today']
+print json_data[u'last_streak'][u'last_reached_goal']
+
+#pprint.pprint(json_data[u'language_data'][u'ru'][u'next_lesson'], indent=4)
+#pprint.pprint(json_data[u'language_data'][u'ru'][u'skills'][0], indent=4)
+"""
+
+import config
+import requests
+import logging
+import pymongo
+import os
+from telegram.ext import Updater
+from telegram import ParseMode
+
+MAX_REQUEST_RETRIES = 3
+
+def get_user_data(duolingo_username):
+    response = None
+    logging.info("Fetching %s duolingo user data", duolingo_username)
+
+    for i in range(1, MAX_REQUEST_RETRIES + 1):
+        try:
+            logging.info("Trying to get %s user data grades, attempt %s of %s" % (duolingo_username, i, MAX_REQUEST_RETRIES))
+            response = requests.get('https://www.duolingo.com/api/1/users/show?username=' + duolingo_username, timeout=1)
+            break
+        except requests.exceptions.Timeout as timeout_err:
+            if i != MAX_REQUEST_RETRIES:
+                logging.info("Timeout reached. Retrying...")
+            else:
+                logging.info("Maximum attempts reached. Exiting.")
+                return None
+
+    return response.json()
+
+
+logging.basicConfig(filename=os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir)) + "/" + 'log.txt',
+					filemode='a',
+					level=logging.INFO,
+					format="%(asctime)s %(levelname)s %(module)s %(message)s",
+					datefmt='%Y-%m-%d %H:%M:%S %Z')
+
+dp = Updater(config.bot_token).dispatcher
+mongo_client = pymongo.MongoClient()
+mongo_db = mongo_client.duolingo_telegram_bot
+users = mongo_db.users_data.find({})
+mongo_client.close()
+
+for user in users:
+    data = get_user_data(user[u'duolingo_username'])
+    if not data:
+        logging.error("Could not get data for duolingo user %s (chat id %s)", user[u'duolingo_username'], user[u'chat_id'])
+    elif data[u'streak_extended_today']:
+        logging.info("Duolingo user %s (chat id %s) has done a good job today!", user[u'duolingo_username'], user[u'chat_id'])
+    else:
+        dp.bot.send_message(chat_id=user[u'chat_id'],
+                            text="""Hi! You haven\'t completed your daily goal for today yet...
+continue your <b>%s</b> day streak.""" % data[u'site_streak'],
+                            parse_mode=ParseMode.HTML)
+        logging.info("Sent a reminder to duolingo user %s (chat id %s)", user[u'duolingo_username'], user[u'chat_id'])
+
+
+
---
+++ service/messages_handler.py
@@ -0,0 +1,95 @@
+import config
+import re
+import pymongo
+import logging
+import os
+from telegram.ext import Updater, CommandHandler
+
+SET_USERNAME_REGEXP = re.compile("\/set_username (\w+) *")
+
+# /start handler
+def start(bot, update):
+    update.message.reply_text("""Hello! I am Duolingo Reminder Bot.
+I will remind you to learn in Duolingo every day.
+
+Let's get started by telling me what is your Duolingo username:
+Please type /set_username <your_duolingo_username>
+
+Type /help for help""")
+    logging.info("/start message sent")
+
+# /help handler
+def help(bot, update):
+    update.message.reply_text("""My available commands:
+/help - to show this help message
+/set_username <your_duolingo_username> - to let me know what is your duolingo username""")
+    logging.info("/help message sent")
+
+# /set_username handler
+def set_username(bot, update):
+
+    match = SET_USERNAME_REGEXP.match(update.message.text)
+    if not match:
+        update.message.reply_text('Invalid command structure.\nUsage: /set_username <duolingo_username>')
+    else:
+        duolingo_username = match.groups()[0]
+
+        mongo_client = pymongo.MongoClient()
+        mongo_db = mongo_client.duolingo_telegram_bot
+
+        mongo_db.users_data.update(
+            {
+                "chat_id": update.message.chat_id
+            },
+            {
+                "$set": {
+                    "duolingo_username": duolingo_username
+                },
+                "$setOnInsert": {
+                    "chat_id": update.message.chat_id,
+                    "duolingo_username": duolingo_username
+                }
+            },
+            upsert=True)
+
+        mongo_client.close()
+
+        update.message.reply_text('Username successfully saved as %s, thank you!' % duolingo_username)
+        logging.info("/set_username: Chat id %s set username to %s", update.message.chat_id, duolingo_username)
+
+# Errors handler
+def error(bot, update, error):
+    logging.warning('Update "%s" caused error "%s"' % (update, error))
+
+
+logging.basicConfig(filename=os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir)) + "/" + 'log.txt',
+					filemode='a',
+					level=logging.INFO,
+					format="%(asctime)s %(levelname)s %(module)s %(message)s",
+					datefmt='%Y-%m-%d %H:%M:%S %Z')
+
+logging.info("**************** Starting service ****************")
+
+# Create the Updater and pass it your bot's token.
+updater = Updater(config.bot_token)
+
+# Get the dispatcher to register handlers
+dp = updater.dispatcher
+
+# on different commands - answer in Telegram
+dp.add_handler(CommandHandler("start", start))
+dp.add_handler(CommandHandler("help", help))
+dp.add_handler(CommandHandler("set_username", set_username))
+
+# TODO Add "remind me in ..." option
+# log all errors
+dp.add_error_handler(error)
+
+logging.info("Starting to listen")
+# Start the Bot
+updater.start_polling()
+
+# Block until the user presses Ctrl-C or the process receives SIGINT,
+# SIGTERM or SIGABRT. This should be used most of the time, since
+# start_polling() is non-blocking and will stop the bot gracefully.
+updater.idle()
